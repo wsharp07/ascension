6ce8ac9ce8d7f6a8a3136e7286b10d28721e137e
William Sharp <wsharp@ltu.edu>
2016-10-28 20:48:41 -0400

    wiring up the dashboard servers widget to obtain data from the api

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,10 @@
 defmodule Ascension.PageView do
   use Ascension.Web, :view
+
+  defp get_servers() do
+    AscensionService.start
+
+    AscensionService.get!("/api/servers").body[:data] 
+    |> Enum.map(fn(x) -> Map.get(x,"attributes") end)
+  end
 end
